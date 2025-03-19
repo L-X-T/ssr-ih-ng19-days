0a48d35cab46d47bc5fad8dc920e13c4b75da610
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-03-19 16:45:46 +0100

    add incremtal hydration

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, ElementRef, inject, NgZone, signal } from '@angular/core';
 
 @Component({
   selector: 'app-base',
   imports: [],
   template: '',
+  host: { '[class.is-hydrated]': 'isHydrated()' },
 })
 export class BaseComponent {
+  private readonly elementRef = inject(ElementRef);
+  readonly isHydrated = signal(false);
+
+  constructor() {
+    afterNextRender(() => {
+      this.blink();
+      this.isHydrated.set(true);
+      console.log(this.constructor.name + ' hydrated');
+    });
+  }
+
   protected onClick(event: Event): void {
     const img = event.target as HTMLImageElement;
     console.log(img.alt + ' was clicked');
+  }
+
+  private blink(): void {
+    // Just a hack used to visualize the hydration for 0.5 seconds
+    this.elementRef.nativeElement.firstChild.style.display = 'none';
+    setTimeout(() => {
+      this.elementRef.nativeElement.firstChild.style.display = '';
+    }, 500);
   }
 }
