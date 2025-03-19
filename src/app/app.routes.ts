ba103f247c281a451ef9d20f6acf3139787c6ab5
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-03-19 15:21:00 +0100

    add demo grid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DemoComponent,
+    // loadComponent: () => import('./demo/demo.component'),
+  },
+];
