152d8d17fed19f919dbecc1fa7f75a35533579b2
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-03-20 11:52:00 +0100

    add @defer

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, signal } from '@angular/core';
 import { VictorComponent } from '../victor/victor.component';
 import { NoemieComponent } from '../noemie/noemie.component';
 import { MarcusComponent } from '../marcus/marcus.component';
@@ -10,6 +10,14 @@ import { JustineComponent } from '../justine/justine.component';
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.scss',
 })
-export class DemoComponent {}
+export class DemoComponent {
+  showJustine = signal(false);
+
+  constructor() {
+    afterNextRender(() => {
+      setTimeout(() => this.showJustine.set(true), 4_200);
+    });
+  }
+}
 
 export default DemoComponent;
